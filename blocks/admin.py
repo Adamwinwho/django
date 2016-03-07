43e812afb2f7c500aa61b2acb49750b891df4c95
adamhu <hsforeveryoung@gmail.com>
2016-03-07 22:36:52 +0800

    添加block模块,并创建管理后台

--- blocks/admin.py
+++ blocks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from models import Blocks
+
+class BlocksAdmin(admin.ModelAdmin):
+    list_display = ("name","desc","owner","last_update_timestamp")
+    list_filter = ("name",)
+
+admin.site.register(Blocks,BlocksAdmin)
--- blocks/models.py
+++ blocks/models.py
@@ -6,10 +6,17 @@ from django.db import models
 # Create your models here.
 
 class Blocks(models.Model):
-    name = models.CharField(max_length=30)
-    desc = models.CharField(max_length=120)
+    name = models.CharField(u"板块名称",max_length=30)
+    desc = models.CharField(u"板块描述",max_length=120)
     owner = models.ForeignKey(User,verbose_name=u"管理员")
 
     create_timestamp = models.DateTimeField(auto_now_add=True)
     last_update_timestamp = models.DateTimeField(auto_now=True)
 
+    def __unicode__(self):
+        return self.name
+
+    #表示当前表,表示的含义,中文显示
+    class Meta:
+        verbose_name = u"板块"
+        verbose_name_plural = u"板块"
