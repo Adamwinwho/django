b396eecf58f2883ab174e9f645906c4af42b847a
adamhu <hsforeveryoung@gmail.com>
2016-03-14 22:43:53 +0800

    渲染输出第一个页面

--- blocks/views.py
+++ blocks/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from models import Blocks
 
 # Create your views here.
+
+def blocks_list(request):
+    blocks = Blocks.objects.all().order_by("-id")
+    return render_to_response("blocks_list.html",{"blocks":blocks})
