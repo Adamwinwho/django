a52be42b395c4bc17b846851398da2141dd7a987
adamhu <hsforeveryoung@gmail.com>
2016-03-24 22:41:59 +0800

    添加文章发表功能

--- articles/urls.py
+++ articles/urls.py
@@ -5,4 +5,5 @@ from django.conf.urls import url,include
 urlpatterns = [
         url(r'^lists/(?P<block_id>\d+)','articles.views.article_list',name='article_list'),
         url(r'^detail/(?P<article_id>\d+)','articles.views.article_detail',name='article_detail'),
+        url(r'^create/(?P<block_id>\d+)','articles.views.article_create',name='article_create'),
     ]
